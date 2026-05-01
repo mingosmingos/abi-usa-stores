6209466b502229e62493c9fbf80c3709ec0d2ee0
mingosmingos <jdomingoscorreiab@gmail.com>
2026-05-01 19:58:08 +0100

    VAR and rolling window

--- Testing2.R
+++ Testing2.R
@@ -11,7 +11,6 @@ data <- na.omit(data)
 K <- 7
 LTS <- K #Lead Time Steps
 S <- round(K/3)
-Runs <- 8
 
 Num_Customers <- data[, "Num_Customers"]
 Sales <- data[, "Sales"]
@@ -20,12 +19,14 @@ Pct_On_Sale <- data[, "Pct_On_Sale"]
 TouristEvent <- data[, "TouristEvent"] 
 
 L <- length(Num_Customers)
-W <- (L - LTS) - (Runs - 1)*S
+W <- round(L * 0.7)
+Runs <- floor((L - W - LTS) / S) + 1
 
 cdata <- cbind(Num_Customers, Sales)
 exogen <- cbind(Num_Employees, Pct_On_Sale, TouristEvent)
 
 predictions <- vector("list", Runs)
+actuals <- vector("list", Runs)
 
 for(i in 1:Runs)
 {
@@ -37,7 +38,41 @@ for(i in 1:Runs)
   
   model <- autoVAR(mtr2, season = K, exogen = exotr)
   predictions[[i]] <- forecastVAR(model, h = LTS, exogen = exots)
-  
-  PRED1 <- predictions[[i]][[1]]
-  PRED2 <- predictions[[i]][[2]]
+  actuals[[i]] <- Y
 }
+
+predictions_df <- data.frame(
+  Num_Customers = unlist(lapply(predictions, `[[`, 1)),
+  Sales         = unlist(lapply(predictions, `[[`, 2))
+)
+
+actual_df <- data.frame(
+  Run           = rep(1:Runs, each = LTS),
+  Step          = rep(1:LTS, times = Runs),
+  Num_Customers = unlist(lapply(actuals, function(y) y[, "Num_Customers"])),
+  Sales         = unlist(lapply(actuals, function(y) y[, "Sales"]))
+)
+
+predictions_df <- data.frame(
+  Run           = rep(1:Runs, each = LTS),
+  Step          = rep(1:LTS, times = Runs),
+  Num_Customers = unlist(lapply(predictions, `[[`, 1)),
+  Sales         = unlist(lapply(predictions, `[[`, 2))
+)
+
+# Plot using base R
+par(mfrow = c(1, 2))
+
+plot(actual_df$Num_Customers, type = "l", col = "black",
+     main = "Num_Customers: Actual vs Predicted",
+     ylab = "Num_Customers", xlab = "Time Step")
+lines(predictions_df$Num_Customers, col = "blue", lty = 2)
+legend("topleft", legend = c("Actual", "Predicted"),
+       col = c("black", "blue"), lty = c(1, 2))
+
+plot(actual_df$Sales, type = "l", col = "black",
+     main = "Sales: Actual vs Predicted",
+     ylab = "Sales", xlab = "Time Step")
+lines(predictions_df$Sales, col = "red", lty = 2)
+legend("topleft", legend = c("Actual", "Predicted"),
+       col = c("black", "red"), lty = c(1, 2))
